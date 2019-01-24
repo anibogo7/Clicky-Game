2f8b34fa8e36854817d4e31415aac0122a7fcbf7
anibogo7 <a.bogosian7@gmail.com>
2019-01-24 14:22:47 -0800

    Updates

---
+++ precache-manifest.91a60192f638d8a61432b269866fc937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "aa3f4b75eae25052e75c",
+    "url": "/Clicky-Game/static/js/main.aa3f4b75.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "aa3f4b75eae25052e75c",
+    "url": "/Clicky-Game/static/css/main.52efced4.chunk.css"
+  },
+  {
+    "revision": "6e83210f8274ef49d04224d46aeaf149",
+    "url": "/Clicky-Game/index.html"
+  }
+];
